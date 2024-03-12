91d94a8ac63f3c6e2613c6c1960826dc290bbd53
gianlucanied <gianluca.nieddu96@gmail.com>
2024-03-12 11:14:52 +0100

    fix: profiletableseeder

--- database/seeders/ProfileTableSeeder.php
+++ database/seeders/ProfileTableSeeder.php
@@ -1,12 +1,9 @@
 <?php
-
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Profile;
 use App\Models\User;
-
 
 class ProfileTableSeeder extends Seeder
 {
@@ -17,28 +14,13 @@ class ProfileTableSeeder extends Seeder
      */
     public function run()
     {
-        // Profile :: factory()
-        // ->count(20)
-        // ->make()
-        // ->each(function($profile) {
-
-        //     $user = User::inRandomOrder()-> limit(1) ->first();
-
-        //     $profile -> user() -> associate($user->id);
-
-        //     $profile -> save();
-        // });
-
         User::all()->each(function($user) {
             // Controlla se l'utente ha già un profilo associato
             if (!$user->profile) {
-                Profile::factory()->make()->each(function($profile) {
-                    $user = User::inRandomOrder() ->first();
-
-                        $profile -> user() -> associate($user->id);
-            
-                        $profile -> save();
-            });
+                // Crea un profilo associato a questo utente
+                $profile = Profile::factory()->make();
+                $profile->user()->associate($user);
+                $profile->save();
             }
         });
     }
