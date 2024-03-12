be1810c41562dd8bca4dfe83b919755afa905e51
alessandrorumi <rumi.alessandro21@gmail.com>
2024-03-12 12:55:05 +0100

    Update

--- front-end/BTrainer-front/src/App.vue
+++ front-end/BTrainer-front/src/App.vue
@@ -1,15 +1,12 @@
 <script setup>
 import AppHeader from './components/AppHeader.vue';
 import AppFooter from './components/AppFooter.vue';
-import AppTrainerGallery from './components/AppTrainerGallery.vue';
 </script>
 
 <template>
-    <AppTrainerGallery/>
     <AppHeader/>
     <router-view></router-view>
     <AppFooter/>
-    
 </template>
 
 <style lang="scss">
--- front-end/BTrainer-front/src/pages/Home.vue
+++ front-end/BTrainer-front/src/pages/Home.vue
@@ -1,13 +1,16 @@
 <script>
 import Jumbo from '../components/Jumbo.vue'
+import AppTrainerGallery from '../components/AppTrainerGallery.vue';
 
 export default {
   components: {
     Jumbo,
+    AppTrainerGallery
   }
 }
 </script>
 
 <template>
     <Jumbo/>
+    <AppTrainerGallery/>
 </template>
