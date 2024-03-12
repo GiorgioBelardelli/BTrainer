c6a1fe7dbb365cdd35859676a9df41e8e057062b
ChrisGarozzo <garozzochristian@gmail.com>
2024-03-12 17:29:32 +0100

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<<<<<<< HEAD
 
 <a href="{{route('user.create')}}">CREA USER</a>
 
-=======
 <div class="container">
     <h1>Users:</h1>
->>>>>>> 08936ad44da7b200c0c4c70cd5981fafdb0be45d
 <ul>
         @foreach ($users as $user)
             <li>
