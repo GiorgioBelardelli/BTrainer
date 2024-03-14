aa273b6c1d1d4f1cb7eb71b848260da329531642
gianlucanied <gianluca.nieddu96@gmail.com>
2024-03-14 15:43:53 +0100

    fix: validations

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,55 +2,50 @@
 
 @section('content')
 
-        @if ($errors->any())
-            <div class="alert alert-danger">    
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+<form action="{{ route('profile.store') }}" method="POST" class="container text-center">
+
+    @csrf
+    @method('POST')
+
+    <div class="shadow-sm card w-50 mx-auto mt-4">
+        <div class="card-body">
+            <div class="mb-3">
+                <label for="phone_number" class="form-label"><strong>Phone Number</strong></label>
+                <input type="text" class="form-control" name="phone_number" id="phone_number" placeholder="Inserisci phone_number">
             </div>
-        @endif
-    <form action="{{ route('profile.store') }}" method="POST" class="container text-center">
 
-        @csrf
-        @method('POST')
-
-        <div class="shadow-sm card w-50 mx-auto mt-4">
-            <div class="card-body">
-                <div class="mb-3">
-                    <label for="phone_number" class="form-label"><strong>Phone Number</strong></label>
-                    <input type="text" class="form-control" name="phone_number" id="phone_number"
-                        placeholder="Inserisci phone_number">
-                </div>
-
-                <div class="mb-3">
-                    <label for="photo" class="form-label"><strong>Photo</strong></label>
-                    <input type="text" class="form-control" name="photo" id="photo"
-                        placeholder="Inserisci photo">
-                </div>
-
-                <div class="mb-3">
-                    <label for="curriculum" class="form-label"><strong>curriculum Utente</strong></label>
-                    <input type="text" class="form-control" name="curriculum" id="curriculum"
-                        placeholder="Inserisci curriculum">
-                </div>
-
-                <div class="mb-3">
-                    <label for="plan_program" class="form-label"><strong>plan_program</strong></label>
-                    <input type="text" class="form-control" name="plan_program" id="plan_program"
-                        placeholder="Inserisci plan_program User">
-                </div>
-
-                <div class="mb-3">
-                    <label for="work_address" class="form-label"><strong>Work Address Utente</strong></label>
-                    <input type="text" class="form-control" name="work_address" id="work_address"
-                        placeholder="Inserisci Work Address User">
-                </div>
-
+            <div class="mb-3">
+                <label for="photo" class="form-label"><strong>Photo</strong></label>
+                <input type="text" class="form-control" name="photo" id="photo" placeholder="Inserisci photo">
             </div>
+
+            <div class="mb-3">
+                <label for="curriculum" class="form-label"><strong>curriculum Utente</strong></label>
+                <input type="text" class="form-control" name="curriculum" id="curriculum" placeholder="Inserisci curriculum">
+            </div>
+
+            <div class="mb-3">
+                <label for="plan_program" class="form-label"><strong>plan_program</strong></label>
+                <input type="text" class="form-control" name="plan_program" id="plan_program" placeholder="Inserisci plan_program User">
+            </div>
+
+            <div class="mb-3">
+                <label for="work_address" class="form-label"><strong>Work Address Utente</strong></label>
+                <input type="text" class="form-control" name="work_address" id="work_address" placeholder="Inserisci Work Address User">
+            </div>
+
         </div>
+    </div>
 
-        <input class="my-1 btn btn-success mt-4" type="submit" value="Crea">
-    </form>
+    <input class="my-1 btn btn-success mt-4" type="submit" value="Crea">
+</form>
 @endsection
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,45 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Edit: </h1>
-        @if ($errors->any())
-            <div class="alert alert-danger">    
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        <form method="POST">
-
-            @csrf
-            @method('PUT')
-
-
-
-            <label for="phone_number" class="form-label"><strong>Phone Number</strong></label>
-            <input type="text" class="form-control" name="phone_number" id="phone_number" value="{{$profile->phone_number}}">
-            <br>
-
-            <label for="photo">photo</label>
-            <input type="text" name="photo" id="photo" value="{{ $profile->photo }}">
-            <br>
-
-            <label for="curriculum">curriculum</label>
-            <input type="text" name="curriculum" id="curriculum" value="{{ $profile->curriculum }}">
-            <br>
-
-            <label for="plan_program">plan_program</label>
-            <input type="text" name="plan_program" id="plan_program" value="{{ $profile->plan_program }}">
-            <br>
-
-            <label for="work_address">Work Address</label>
-            <input type="text" name="work_address" id="work_address" value="{{ $profile->work_address }}">
-            <br>
-
-            <input type="submit" value="Update">
-        </form>
+<div class="container">
+    <h1>Edit: </h1>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
+    @endif
+    <form method="POST">
+
+        @csrf
+        @method('PUT')
+
+
+
+        <label for="phone_number" class="form-label"><strong>Phone Number</strong></label>
+        <input type="text" class="form-control" name="phone_number" id="phone_number" value="{{$profile->phone_number}}">
+        <br>
+
+        <label for="photo">photo</label>
+        <input type="text" name="photo" id="photo" value="{{ $profile->photo }}">
+        <br>
+
+        <label for="curriculum">curriculum</label>
+        <input type="text" name="curriculum" id="curriculum" value="{{ $profile->curriculum }}">
+        <br>
+
+        <label for="plan_program">plan_program</label>
+        <input type="text" name="plan_program" id="plan_program" value="{{ $profile->plan_program }}">
+        <br>
+
+        <label for="work_address">Work Address</label>
+        <input type="text" name="work_address" id="work_address" value="{{ $profile->work_address }}">
+        <br>
+
+        <input type="submit" value="Update">
+    </form>
+</div>
 @endsection
