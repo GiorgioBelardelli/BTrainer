55b531389ead759106770839898c5bab62c27ec1
gianlucanied <gianluca.nieddu96@gmail.com>
2024-03-15 17:15:26 +0100

    fix: bug dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,33 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h2 class="fs-4 text-secondary my-4">
-            {{ __('Dashboard') }}
-        </h2>
-        <div class="row justify-content-center">
-            <div class="col">
-                <div class="card">
-                    <div class="card-header">{{ __('User Dashboard') }}</div>
+<div class="container">
+    <h2 class="fs-4 text-secondary my-4">
+        {{ __('Dashboard') }}
+    </h2>
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">{{ __('User Dashboard') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
-                        {{-- @if (Auth::user()->id === $profile->id) --}}
-                            <h1>Vuoi crearti un profilo da Trainer?</h1>
-                            <button><a href="{{ route('profile.create') }}" class="btn mb-3">CREA PROFILO</a></button>
-                        {{-- @endif --}}
-
-                        <h1>{{$profile->phone_number}}</h1>
-
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
                     </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+
+                    {{-- Verifica se l'utente ha un profilo --}}
+                    @if (!$userProfile)
+                    <h1>Vuoi crearti un profilo da Trainer?</h1>
+                    <button><a href="{{ route('profile.create') }}" class="btn mb-3">CREA PROFILO</a></button>
+                    @endif
+
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
