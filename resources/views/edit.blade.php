08d0f9008d1ec74920d77ab30990d5fef3f5d849
ChrisGarozzo <garozzochristian@gmail.com>
2024-03-16 12:21:50 +0100

    fix form (@endauth & @endif)

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,92 +1,92 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>Modifica il tuo Profilo: </h1>
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
-    <form method="POST" enctype="multipart/form-data">
-
-
-        @auth
-            @if (Auth::user()->id === $profile->user_id)
-                <form method="POST" enctype="multipart/form-data">
-
-            @csrf
-            @method('PUT')
-
-            <div class="shadow-sm card w-50 mx-auto mt-4">
-        <div class="card-body">
-            <div class="mb-3">
-                <label for="phone_number" class="form-label"><strong>Numero di Telefono</strong></label>
-                <input required type="text" class="form-control" name="phone_number" id="phone_number" value="{{$profile->phone_number}}">
+    <div class="container">
+        <h1>Modifica il tuo Profilo: </h1>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
+        @endif
+        <form method="POST" enctype="multipart/form-data">
 
-                    <div class="mb-3">
-                        <label for="photo" class="form-label"><strong>Foto</strong></label>
-                        <input required type="file" class="form-control" name="photo" id="photo"
-                            value="{{ $profile->photo }}" accept="image/*">
-                    </div>
-                    <div class="img">
-                        <img class="w-100" src="{{ asset('storage/' . $profile->photo) }}" alt="">
-                    </div>
 
-                    <div class="mb-3">
-                        <label for="curriculum" class="form-label"><strong>Curriculum</strong></label>
-                        <input required type="file" class="form-control" name="curriculum" id="curriculum"
-                            accept=".pdf, .png, .jpg, .jpeg" value="{{ $profile->curriculum }}">
-                    </div>
+            @auth
+                @if (Auth::user()->id === $profile->user_id)
+                    <form method="POST" enctype="multipart/form-data">
 
-                    <div class="mb-3">
-                        <label for="plan_program" class="form-label"><strong>Programma</strong></label>
-                        <textarea required class="form-control" name="plan_program" id="plan_program">{{ $profile->plan_program }}</textarea>
-                    </div>
+                        @csrf
+                        @method('PUT')
 
-                    <div class="mb-3">
-                        <label for="work_address" class="form-label"><strong>Indirizzo</strong></label>
-                        <input required type="text" class="form-control" name="work_address" id="work_address"
-                            value="{{ $profile->work_address }}">
-                    </div>
+                        <div class="shadow-sm card w-50 mx-auto mt-4">
+                            <div class="card-body">
+                                <div class="mb-3">
+                                    <label for="phone_number" class="form-label"><strong>Numero di Telefono</strong></label>
+                                    <input required type="text" class="form-control" name="phone_number" id="phone_number"
+                                        value="{{ $profile->phone_number }}">
+                                </div>
 
-            <div class="techno mb-3">
-                <h5>Specializzazioni:</h5>
-                <div class="row">
-                    @foreach ($specializations as $specialization)
-                        <div class="col-md-6">
-                            <input required 
-                                type="checkbox" 
-                                name="specialization_id[]" 
-                                value="{{ $specialization->id }}"
-                                @foreach ($profile->specializations as $profile_specialization)
+                                <div class="mb-3">
+                                    <label for="photo" class="form-label"><strong>Foto</strong></label>
+                                    <input required type="file" class="form-control" name="photo" id="photo"
+                                        value="{{ $profile->photo }}" accept="image/*">
+                                </div>
+                                <div class="img">
+                                    <img class="w-100" src="{{ asset('storage/' . $profile->photo) }}" alt="">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="curriculum" class="form-label"><strong>Curriculum</strong></label>
+                                    <input required type="file" class="form-control" name="curriculum" id="curriculum"
+                                        accept=".pdf, .png, .jpg, .jpeg" value="{{ $profile->curriculum }}">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="plan_program" class="form-label"><strong>Programma</strong></label>
+                                    <textarea required class="form-control" name="plan_program" id="plan_program">{{ $profile->plan_program }}</textarea>
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="work_address" class="form-label"><strong>Indirizzo</strong></label>
+                                    <input required type="text" class="form-control" name="work_address" id="work_address"
+                                        value="{{ $profile->work_address }}">
+                                </div>
+
+                                <div class="techno mb-3">
+                                    <h5>Specializzazioni:</h5>
+                                    <div class="row">
+                                        @foreach ($specializations as $specialization)
+                                            <div class="col-md-6">
+                                                <input required type="checkbox" name="specialization_id[]"
+                                                    value="{{ $specialization->id }}"
+                                                    @foreach ($profile->specializations as $profile_specialization)
                                     @if ($specialization->id == $profile_specialization->id)
                                         checked
-                                    @endif
-                                @endforeach
-                            >
-                            <label class="checkbox-inline" id="spec-white"> {{ $specialization->name }}</label>
+                                    @endif @endforeach>
+                                                <label class="checkbox-inline" id="spec-white">
+                                                    {{ $specialization->name }}</label>
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                </div>
+                                <input class="my-1 btn mt-4 mx-auto" type="submit" value="Update" id="yellow">
+                            </div>
                         </div>
-                    @endforeach
-                </div>
-            </div>
-            <input class="my-1 btn mt-4 mx-auto" type="submit" value="Update" id="yellow">
-        </div>
-    </div>
 
-    </form>
-</div>
+                    </form>
+                @endif
+            @endauth
+    </div>
 @endsection
 
 <style lang=scss scoped>
-    .form-label strong{
+    .form-label strong {
         color: white;
-        margin:auto;
+        margin: auto;
     }
 
     .card-body {
@@ -94,36 +94,37 @@
             margin: auto;
         }
     }
+
     .techno {
         h5 {
-            color:white;
+            color: white;
         }
     }
 
     #spec-white {
-        color:white;
+        color: white;
     }
 
     .container h1 {
-        color:white;
+        color: white;
         font-size: 30px;
     }
 
     .card-body {
         margin: auto;
 
-        input{
+        input {
             margin: auto;
         }
 
-        #yellow{
-            color:black;
+        #yellow {
+            color: black;
             background-color: yellow;
         }
-        #yellow:hover {
-            transform:scale(1.1);
-        }
-    
-    }
 
+        #yellow:hover {
+            transform: scale(1.1);
+        }
+
+    }
 </style>
