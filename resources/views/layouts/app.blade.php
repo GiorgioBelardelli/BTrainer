a1b4ef84dc03952cc033ba31fc4fddcc60ee2319
ChrisGarozzo <garozzochristian@gmail.com>
2024-03-18 21:52:26 +0100

    added: title B-Trainer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    {{-- <title>{{ config('app.name', 'BTrainer') }}</title> --}}
+    <title>B-Trainer</title>
 
 
     <!-- Fonts -->
