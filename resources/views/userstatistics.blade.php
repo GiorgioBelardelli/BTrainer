47fbd38b464756ea7d28692ce670fceb03103428
gianlucanied <gianluca.nieddu96@gmail.com>
2024-03-25 17:39:38 +0100

    add: list for reviews and messages

--- resources/views/userstatistics.blade.php
+++ resources/views/userstatistics.blade.php
@@ -32,7 +32,30 @@
                     <div>
                         <h1>Numero recensioni: {{count($userProfile->reviews)}}</h1>
                         <h1>Numero messaggi: {{count($userProfile->messages)}}</h1>
-                        <canvas id="myChart"></canvas>
+                    </div>
+                    <div>
+                        <div>
+                            <h2>Recensioni per mese:</h2>
+                            <ul>
+                                <?php \Carbon\Carbon::setLocale('it'); ?>
+                                @foreach($userProfile->reviews->groupBy(function($review) {
+                                return \Carbon\Carbon::parse($review->date)->format('F Y');
+                                }) as $month => $reviews)
+                                <li>{{ $month }}: {{ count($reviews) }} recensioni</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        <div>
+                            <h2>Messaggi per mese:</h2>
+                            <ul>
+                                <?php \Carbon\Carbon::setLocale('it'); ?>
+                                @foreach($userProfile->messages->groupBy(function($message) {
+                                return \Carbon\Carbon::parse($message->date)->locale('it')->format('F Y');
+                                }) as $month => $messages)
+                                <li>{{ $month }}: {{ count($messages) }} messaggi</li>
+                                @endforeach
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -41,26 +64,6 @@
 
 
     <script>
-        const ctx = document.getElementById('myChart');
-
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [5, 19, 3, 5, 2, 3],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
     </script>
 
     @endsection
