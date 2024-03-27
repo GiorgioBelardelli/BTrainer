99f5620907bbe61f9a1bbc461b5063360a22d33a
gianlucanied <gianluca.nieddu96@gmail.com>
2024-03-27 17:25:40 +0100

    fix: css login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-4">
+<div class="container ms-container-login">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="mb-4 row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo E-mail') }}</label>
+                            <label for="email" class="label-login col-md-4 col-form-label text-md-right">{{ __('Indirizzo E-mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -26,10 +26,10 @@
                         </div>
 
                         <div class="mb-4 row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="label-login col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="label-login form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -44,7 +44,7 @@
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
+                                    <label class="label-login form-check-label" for="remember">
                                         {{ __('Ricordami') }}
                                     </label>
                                 </div>
@@ -74,34 +74,50 @@
 
 
 <style lang=scss scoped>
+    @use '../styles/partials/variables' as *;
 
-@use '../styles/partials/variables' as *;
+    .ms-container-login {
+        background-color: #FFCC00;
+        height: 80vh;
+        padding-top: 5vh;
+    }
+
+    .label-login {
+        color: black;
+    }
 
     strong {
-            color: white;
+        color: white;
     }
 
     form .btn {
-            color:black;
-            background-color:yellow;
-        }
+        color: black;
+        background-color: #FFCC00;
+        margin: 0 10px 0 0;
+    }
 
-        .nav-item .nav-link:hover {
-            color: white;
-        }
+
+    .nav-item .nav-link:hover {
+        color: white;
+    }
 
     form .btn:hover {
-        transform:scale(1.2);
-        color:black;
-        background-color:yellow;
+        transform: scale(1.1);
+        transition: 0.5s;
+        color: black;
+        background-color: #FFCC00;
+    }
+
+    form .btn:not(:hover) {
+        transform: scale(1);
+        transition: 0.5s;
     }
 
     form ul li a:hover {
-        color:yellow;
+        color: yellow;
     }
 
     label {
-        color:white;
+        color: white;
     }
-
 </style>
